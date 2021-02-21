f81904d8134d6962d6f60ffa3d58dbfa26c3084c
Shital More <moreshital.engg@gmail.com>
2021-02-21 19:30:32 +0530

    Added first commit

--- scripts/startup-script-app.sh
+++ scripts/startup-script-app.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-get update -y
+sudo apt-get install nginx nginx-common -y
 source ~/.profile && source ~/.nvm/nvm.sh && source ~/.bashrc
 source /home/ubuntu/google-cloud-sdk/completion.bash.inc && source /home/ubuntu/google-cloud-sdk/path.bash.inc
 sleep 60s
