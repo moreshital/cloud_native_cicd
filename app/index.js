e962b760dab4d0cf879bfc861edf9159dc513896
Shital More <moreshital.engg@gmail.com>
2021-02-25 00:16:18 +0530

    env

--- app/index.js
+++ app/index.js
@@ -4,16 +4,16 @@ var app = express()
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.render('home')
+  res.render(process.env.NODE_ENV)
 });
 
 app.get('/test', function (req, res) {
   res.send('Test is passed')
 })
 
-app.get('/environment', function (req, res) {
- res.render(process.env.NODE_ENV)
-})
+//app.get('/environment', function (req, res) {
+// res.render(process.env.NODE_ENV)
+//})
 
 app.listen(3000, function () {
   console.log('Listening on port 3000...')
