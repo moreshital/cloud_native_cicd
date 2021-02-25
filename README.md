8b205b26704fa760de89b54d844ef07bab02a35d
Shital More <moreshital.engg@gmail.com>
2021-02-25 20:42:14 +0530

    added timeout

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ npm test
 npm start
 ```
 
-Latest image running on Prod is wids-xxxx
+Latest image running on Prod is wids-xxx
--- functions/repos.js
+++ functions/repos.js
@@ -4,7 +4,7 @@ module.exports = {
         'channel': 'cloud-native-cicd',
         'deploymentSourceBranch': 'master',
         'productionCandidateBranch': 'production-candidate',
-        'repoLink': 'https://github.com/moreshital/cloud-native-cicd',
+        'repoLink': 'https://github.com/moreshital/cloud-native-cicd/',
         'productionTriggerId': 'wids-cnci-production-deploy-trigger',
         'stagingEnvUrl': 'http://34.120.42.4/',
         'productionEnvUrl': 'http://34.120.63.180/',
