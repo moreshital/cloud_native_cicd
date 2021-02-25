34e0ffd66e77ebdb14add1f064969d5d35ab694b
Shital More <moreshital.engg@gmail.com>
2021-02-25 23:45:12 +0530

    updated code

--- functions/repos.js
+++ functions/repos.js
@@ -1,10 +1,10 @@
 module.exports = {
-    'cloud-native-cicd': {
-        'name': 'cloud-native-cicd',
+    'cloud_native_cicd': {
+        'name': 'cloud_native_cicd',
         'channel': 'cloud-native-cicd',
         'deploymentSourceBranch': 'master',
         'productionCandidateBranch': 'production-candidate',
-        'repoLink': 'https://github.com/moreshital/cloud-native-cicd/',
+        'repoLink': 'https://github.com/moreshital/cloud_native_cicd/',
         'productionTriggerId': 'wids-cnci-production-deploy-trigger',
         'stagingEnvUrl': 'http://34.120.42.4/',
         'productionEnvUrl': 'http://34.120.63.180/',
